2450e3dab69d8567eb43cb8ae7aba7550d822451
Thomas 2 <thomas2@MacBook-Pro-de-Thomas.local>
2018-01-11 15:17:40 +0100

    JS fonction

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+let monTitre = document.querySelector('h1');
+monTitre.textContent = 'Hello world!';
+console.log("coucou ta mere")
+let parfumGlace = 'chocolat';
+// parfumGlace = 'vanille';
+if (parfumGlace === 'chocolat') {
+  alert("J'adore la glace au chocolat !");    
+} else {
+  alert("J'aurai préféré du chocolat.");    
+}
