3641c10bed8b42c70801afb9428cb34772eefa61
BenCmjn <benjamin.calmejane@gmail.com>
2018-01-11 16:02:22 +0100

    ecrire du js qui bug

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,44 @@
-let monTitre = document.querySelector('h1');
-monTitre.textContent = 'Hello world!';
-console.log("coucou ta mere")
-let parfumGlace = 'chocolat';
-// parfumGlace = 'vanille';
-if (parfumGlace === 'chocolat') {
-  alert("J'adore la glace au chocolat !");    
+// let monTitre = document.querySelector('h1');
+// monTitre.textContent = 'Hello world!';
+
+/////////////////////////////////////////////////
+
+// let monImage = document.querySelector('img');
+
+// monImage.onclick = function(){
+// 	let maSrc = monImage.getAttribute("src");
+// 	if (maSrc === "images/firefox-icon.png") {
+// 		monImage.setAttribute ("src","images/chrome_logo.jpg")
+// 	}
+// 	else{
+// 		monImage.setAttribute ("src","images/firefox-icon.png")
+// 	}
+// }
+
+let myButton = document.querySelector("button");
+let monTitre = document.querySelector("h1");
+
+function defUser() {
+	let monNom = prompt("Quel est ton nom ?");
+	localStorage.setItem("nom", monNom);
+	monTitre.innerHTML="Mozilla c'est Cool"+ monNom;
+}
+
+
+if(!localStorage.getItem("nom")) {
+	defUser();
+}else{
+	var recName = localStorage.getItem("nom");
+	monTitre.innerHTML="Mozilla c'est Cool"+ recName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
 } else {
-  alert("J'aurai préféré du chocolat.");    
+  var storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function(){
+	defUser();
 }
